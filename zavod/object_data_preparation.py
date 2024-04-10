e79f4887f731c86ee9c2e67379982c1940c33a94
primera7790 <def.primera@gmail.com>
2024-04-10 15:21:07 +0500

    feature engineering

--- zavod/object_data_preparation.py
+++ zavod/object_data_preparation.py
@@ -38,8 +38,16 @@ def to_objects_split(data):
     # print(split_list)
 
 
-def feature_engineering(data, features):
+def feature_engineering(data, features, train=False, test=False):
     ''' Превращаем данные в набор признаков
+
+    :param data: pandas dataframe, содержащий подготовленные объекты
+    :param features: pandas dataframe, являющийся перечнем желаемых признаков
+    :param train: bool, если подготавливаем данные для обучения - True
+    :param test: bool, если подготавливаем данные для предсказания - True
+    :return: .csv файл
+    '''
+    ''' 
 
     :param data: pandas dataframe, содержащий подготовленные объекты
     :param features: pandas dataframe, являющийся перечнем желаемых признаков
@@ -57,46 +65,68 @@ def feature_engineering(data, features):
         letters_count = 0
         other_symbols_count = 0
         sym_dict = defaultdict(int)
+        feature_values_list = [0] * futures_num
 
         for sym in obj_name:
-            sym_dict[sym] += 1
+            sym_dict[sym.lower()] += 1
 
             alphabet = [chr(i) for i in range(ord('а'), ord('я') + 1)]
+            other_symbols = ['.', ',', '/', '-']
 
-            if sym in range(10):
+            if sym.isdigit() and int(sym) in range(10):
                 figures_count += 1
 
-            elif sym in alphabet:
+            elif sym.lower() in alphabet:
                 letters_count += 1
 
             else:
                 other_symbols_count += 1
 
-            feature_values_list = [0] * futures_num
+            for l_idx in range(len(alphabet)):
+                if alphabet[l_idx] in sym_dict:
+                    feature_values_list[15 + l_idx] = 1
+                    feature_values_list[61 + l_idx] = sym_dict[alphabet[l_idx]]
+                else:
+                    feature_values_list[15 + l_idx] = 0
+                    feature_values_list[61 + l_idx] = 0
 
-        return []
+            for f in range(10):
+                if f in sym_dict:
+                    feature_values_list[5 + f] = 1
+                    feature_values_list[51 + f] = sym_dict[alphabet[f]]
+                else:
+                    feature_values_list[5 + f] = 0
+                    feature_values_list[51 + f] = 0
+
+            for s_idx in range(len(other_symbols)):
+                if other_symbols[s_idx] in sym_dict:
+                    feature_values_list[47 + s_idx] = 1
+                    feature_values_list[93 + s_idx] = sym_dict[other_symbols[s_idx]]
+                else:
+                    feature_values_list[47 + s_idx] = 0
+                    feature_values_list[93 + s_idx] = 0
+
+            feature_values_list[0] = obj_name
+            feature_values_list[1] = symbols_count
+            feature_values_list[2] = figures_count
+            feature_values_list[3] = letters_count
+            feature_values_list[4] = other_symbols_count
+
+        return feature_values_list
 
     features_data = pd.DataFrame(columns=features.index)
-    print(len(features.index))
 
     to_futures = data['object'].apply(lambda x: for_each_object(x, len(features.index)))
 
-    features_data.loc[:] = to_futures
+    for line in to_futures:
+        features_data.loc[len(features_data.index), :] = line
 
-    # for obj in range(len(data['object'])):
-        # object_name = data.loc[obj, 'object']
+    if train:
+        features_data.to_csv('data/total_data/features_train.csv')
+    elif test:
+        features_data.to_csv('data/total_data/features_test.csv')
 
-        # values = for_each_object(object_name)
-
-        # feature_values_list = [0] * len(features_data.columns)
-
-
-        # features_data.loc[len(features_data.index), :] = feature_values_list
-
-        # print(object_name)
-        # break
     return
-
 
 
 def main():
@@ -104,10 +134,10 @@ def main():
         warnings.simplefilter('ignore')
         obj_names_file = pd.read_csv('data/total_data/csv/obj_names.csv', index_col=0)
         obj_split_names_file = pd.read_csv('data/total_data/csv/obj_split_names.csv', index_col=0)
-        obj_features = pd.read_csv('data/total_data/csv/obj_features.csv', index_col=False, header=None)
+        obj_features = pd.read_csv('data/total_data/csv/obj_features.csv', index_col=0, header=None)
 
     # to_objects_split(obj_names_file)
-    feature_engineering(obj_split_names_file, obj_features)
+    feature_engineering(obj_split_names_file, obj_features, train=True)
 
 
 if __name__ == '__main__':
